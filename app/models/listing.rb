d8518d806c54cb226b25d109a04dda9d9e532223
Vidy <vidyalfredo@VIdys-Mac-mini.local>
2015-02-11 23:28:13 +0700

    connect users to listings

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -12,6 +12,8 @@ class Listing < ActiveRecord::Base
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
   validates :name, :description, :price, presence: true
-  validates :price, numericality: ( greater_than: 0)
+  validates :price, numericality: { greater_than: 0 }
   validates_attachment_presence :image
+
+  belongs_to :user
 end
